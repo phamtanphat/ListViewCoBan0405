913b5af55e45847c5d37456c03c75fd98f87f4f3
thuviengithub0208 <khoaphp@yahoo.com>
2018-05-23 18:35:46 +0700

    "Update List View Co Ban"

--- app/src/main/java/com/ptp/phamtanphat/listviewcoban0405/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/listviewcoban0405/MainActivity.java
@@ -2,8 +2,11 @@ package com.ptp.phamtanphat.listviewcoban0405;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -29,6 +32,35 @@ public class MainActivity extends AppCompatActivity {
 
         arrayAdapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1,mangten);
         lv.setAdapter(arrayAdapter);
-        
+//        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+////                Toast.makeText(MainActivity.this, mangten.get(position), Toast.LENGTH_SHORT).show();
+//                mangten.remove(position);
+//                //Chi khi them xoa sua
+//                arrayAdapter.notifyDataSetChanged();
+//            }
+//        });
+//        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+//                mangten.add("Nguyen Van F");
+//                arrayAdapter.notifyDataSetChanged();
+//                return true;
+//            }
+//        });
+        lv.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, mangten.get(position), Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
+
 }
